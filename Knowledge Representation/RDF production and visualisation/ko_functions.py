deaaa2d625a43476b5f0455844a8f8ba09cd9cc9
Liber-R <132198967+Liber-R@users.noreply.github.com>
2024-06-02 17:58:26 +0200

    Update ko_functions.py

--- Knowledge Representation/RDF production and visualisation/ko_functions.py
+++ Knowledge Representation/RDF production and visualisation/ko_functions.py
@@ -18,7 +18,7 @@ path = "<https://w3id.org/BerlinWallProject/"
 literals = ["crm:P45 consists of",
             "crm:P2 has type",
             "fo:description",
-            "crm:P45 consists of",
+            "crm:P45_consists_of",
             "fo:budget",
             "fo:duration",
             "fo:title",
@@ -68,31 +68,11 @@ def rdf_producer(csv_files:list):
             header = next(f)
             for triple in f:
                 subject = triple[0][1:-1]
-                print(subject)
-                o = triple[1]
-                ontology = prefix_dict[o[:o.index(":")]]
+                ontology = prefix_dict[triple[1][:triple[1].index(":")]]
                 predicate = triple[1][triple[1].index(":") + 1:]
                 if triple[2].startswith("<"):
                     object = URIRef(triple[2][1:-1])
                 else:
                     object = Literal(triple[2])
                 rdf_graph.add((URIRef(subject), ontology[predicate], object))
-    rdf_graph.serialize(destination="items.ttl", format="turtle")
-
-def rdf_visualiser(rdf_file):
-
-    # Apri il tuo file .ttl
-    with open(rdf_file, 'r', encoding="utf-8") as file:
-        data = file.read()
-
-    # Invia una richiesta POST al servizio
-    response = requests.post(f"http://www.ldf.fi/service/rdf-grapher?rdf={data}&from=ttl&to=png")
-
-    # Controlla se la richiesta è andata a buon fine
-    if response.status_code == 200:
-        # Salva l'immagine del grafo
-        with open('rdf_graph.png', 'wb') as f:
-            f.write(response.content)
-    else:
-        print("Si è verificato un errore:", response.status_code)
-        
+    rdf_graph.serialize(destination="items.ttl", format="turtle")        
